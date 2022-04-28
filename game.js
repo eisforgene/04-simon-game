b3170f9a234981ea89a77fb59a40ffaa390fc3cc
eisforgene <82473195+eisforgene@users.noreply.github.com>
2022-04-28 13:23:03 -0700

    checks which button is pressed

--- game.js
+++ game.js
@@ -1,9 +1,23 @@
 let buttonColors = ['red', 'blue', 'green', 'yellow'];
 let gamePattern = [];
+let userClickedPattern = [];
 
 function nextSequence() {
     let randomNumber = Math.round((Math.random() * 3));
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeOut(150).fadeIn(150).fadeIn(150);
+
+    var audio = new Audio('sounds/' + randomChosenColor + '.mp3');
+    audio.play();
 }
 
+$('.btn').click(function(e) {
+    let userChosenColor = e.target.id;
+    console.log(userChosenColor);
+
+    userClickedPattern.push(userChosenColor);
+})
+
+nextSequence();
